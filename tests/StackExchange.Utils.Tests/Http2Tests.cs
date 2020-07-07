1059dd5a0be8b05a2075d0ff771445bc0848553e
mgravell <marc.gravell@gmail.com>
2020-07-07 13:20:44 +0100

    tidied

--- tests/StackExchange.Utils.Tests/Http2Tests.cs
+++ tests/StackExchange.Utils.Tests/Http2Tests.cs
@@ -1,15 +1,19 @@
 ﻿#if KESTREL
 using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using RunTestServer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace StackExchange.Utils.Tests
 {
-    public class Http2Tests : IClassFixture<Http2Server>
+    public class Http2Tests : IClassFixture<Http2Tests.Http2Server>
     {
         private readonly Http2Server _server;
         private readonly ITestOutputHelper _log;
@@ -20,59 +24,54 @@ namespace StackExchange.Utils.Tests
             _log = log ?? throw new ArgumentNullException(nameof(log));
         }
 
-        public enum ServerMode
-        {
-            Http1Only,
-            Http2Only,
-            Http1or2,
-            Https1or2,
-        }
-
         [Theory]
         // non-TLS http1: should work (returning 1.1)
-        [InlineData(ServerMode.Http1Only, false, null, "1.1", "HTTP/1.1")]
-        [InlineData(ServerMode.Http1Only, false, "1.1", "1.1", "HTTP/1.1")]
-        [InlineData(ServerMode.Http1Only, false, "2.0", "1.1", "HTTP/1.1")]
+        [InlineData(HttpProtocols.Http1, false, false, null, "1.1", "HTTP/1.1")]
+        [InlineData(HttpProtocols.Http1, false, false, "1.1", "1.1", "HTTP/1.1")]
+        [InlineData(HttpProtocols.Http1, false, false, "2.0", "1.1", "HTTP/1.1")]
 
         // non-TLS http2 without the global override: should always fail
-        [InlineData(ServerMode.Http2Only, false, null, "1.1", "HTTP/1.1", true)]
-        [InlineData(ServerMode.Http2Only, false, "1.1", "1.1", "HTTP/1.1", true)]
-        [InlineData(ServerMode.Http2Only, false, "2.0", "2.0", "HTTP/2", true)]
+        [InlineData(HttpProtocols.Http2, false, false, null, "1.1", "HTTP/1.1", true)]
+        [InlineData(HttpProtocols.Http2, false, false, "1.1", "1.1", "HTTP/1.1", true)]
+        [InlineData(HttpProtocols.Http2, false, false, "2.0", "2.0", "HTTP/2", true)]
 
         // non-TLS http2 with the global override: should work if we specify http2
-        [InlineData(ServerMode.Http2Only, true, null, "1.1", "HTTP/1.1", true)]
-        [InlineData(ServerMode.Http2Only, true, "1.1", "1.1", "HTTP/1.1", true)]
-        [InlineData(ServerMode.Http2Only, true, "2.0", "2.0", "HTTP/2")]
+        [InlineData(HttpProtocols.Http2, false, true, null, "1.1", "HTTP/1.1", true)]
+        [InlineData(HttpProtocols.Http2, false, true, "1.1", "1.1", "HTTP/1.1", true)]
+        [InlineData(HttpProtocols.Http2, false, true, "2.0", "2.0", "HTTP/2")]
 
         // non-TLS http* without the global override: should work, server prefers 1.1
-        [InlineData(ServerMode.Http1or2, false, null, "1.1", "HTTP/1.1")]
-        [InlineData(ServerMode.Http1or2, false, "1.1", "1.1", "HTTP/1.1")]
-        [InlineData(ServerMode.Http1or2, false, "2.0", "1.1", "HTTP/1.1")]
+        [InlineData(HttpProtocols.Http1AndHttp2, false, false, null, "1.1", "HTTP/1.1")]
+        [InlineData(HttpProtocols.Http1AndHttp2, false, false, "1.1", "1.1", "HTTP/1.1")]
+        [InlineData(HttpProtocols.Http1AndHttp2, false, false, "2.0", "1.1", "HTTP/1.1")]
 
         // non-TLS http* with the global override: should work for 1.1; with 2, client and server argue
-        [InlineData(ServerMode.Http1or2, true, null, "1.1", "HTTP/1.1")]
-        [InlineData(ServerMode.Http1or2, true, "1.1", "1.1", "HTTP/1.1")]
-        [InlineData(ServerMode.Http1or2, true, "2.0", "2.0", "HTTP/2", true)]
+        [InlineData(HttpProtocols.Http1AndHttp2, false, true, null, "1.1", "HTTP/1.1")]
+        [InlineData(HttpProtocols.Http1AndHttp2, false, true, "1.1", "1.1", "HTTP/1.1")]
+        [InlineData(HttpProtocols.Http1AndHttp2, false, true, "2.0", "2.0", "HTTP/2", true)]
+
+        // TLS http1: should always work, but http2 attempt is ignored
+        [InlineData(HttpProtocols.Http1, true, false, null, "1.1", "HTTP/1.1")]
+        [InlineData(HttpProtocols.Http1, true, false, "1.1", "1.1", "HTTP/1.1")]
+        [InlineData(HttpProtocols.Http1, true, false, "2.0", "1.1", "HTTP/1.1")]
+
+        // TLS http2: should work as long as we actually send http2
+        [InlineData(HttpProtocols.Http2, true, false, null, "1.1", "HTTP/1.1", true)]
+        [InlineData(HttpProtocols.Http2, true, false, "1.1", "1.1", "HTTP/1.1", true)]
+        [InlineData(HttpProtocols.Http2, true, false, "2.0", "2.0", "HTTP/2")]
 
         // TLS http*: should always work
-        [InlineData(ServerMode.Https1or2, false, null, "1.1", "HTTP/1.1")]
-        [InlineData(ServerMode.Https1or2, false, "1.1", "1.1", "HTTP/1.1")]
-        [InlineData(ServerMode.Https1or2, false, "2.0", "2.0", "HTTP/2")]
-        public async Task UsesVersion(ServerMode mode, bool allowUnencryptedHttp2, string specified, string expectedVersion, string expectedResponse, bool failure = false)
+        [InlineData(HttpProtocols.Http1AndHttp2, true, false, null, "1.1", "HTTP/1.1")]
+        [InlineData(HttpProtocols.Http1AndHttp2, true, false, "1.1", "1.1", "HTTP/1.1")]
+        [InlineData(HttpProtocols.Http1AndHttp2, true, false, "2.0", "2.0", "HTTP/2")]
+        public async Task UsesVersion(HttpProtocols protocols, bool tls, bool allowUnencryptedHttp2, string specified, string expectedVersion, string expectedResponse, bool failure = false)
         {
             bool oldMode = Http.AllowUnencryptedHttp2;
             try
             {
                 Http.AllowUnencryptedHttp2 = allowUnencryptedHttp2;
 
-                var uri = mode switch
-                {
-                    ServerMode.Http1Only => Http2Server.Http1Uri,
-                    ServerMode.Http2Only => Http2Server.Http2Uri,
-                    ServerMode.Http1or2 => Http2Server.Http1or2Uri,
-                    ServerMode.Https1or2 => Http2Server.Https1or2Uri,
-                    _ => throw new ArgumentOutOfRangeException(nameof(mode)),
-                };
+                var uri = _server.GetUri(protocols, tls);
                 Log($"Server is on {uri}; specifying: '{specified}'");
                 var request = Http.Request(uri, new HttpSettings {
                     ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
@@ -115,6 +114,67 @@ namespace StackExchange.Utils.Tests
             {
                 Http.AllowUnencryptedHttp2 = oldMode;
             }
+        }
+
+        public class Http2Server : IDisposable
+        {
+            private readonly IWebHost _host;
+
+            private readonly int[] _ports = Enumerable.Range(10123, 6).ToArray();
+
+            public string GetUri(HttpProtocols protocols, bool tls)
+            {
+                var index = protocols switch
+                {
+                    HttpProtocols.Http1 => tls ? 3 : 0,
+                    HttpProtocols.Http2 => tls ? 4 : 1,
+                    HttpProtocols.Http1AndHttp2 => tls ? 5 : 2,
+                    _ => throw new ArgumentOutOfRangeException(nameof(protocols)),
+                };
+                return $"{(tls ? "https" : "http")}://localhost:{_ports[index]}/";
+            }
+            public Task WaitForShutdownAsync() => _host.WaitForShutdownAsync();
+
+            public Http2Server()
+            {
+                _host = new WebHostBuilder()
+                    .UseKestrel(options => {
+                        options.ListenLocalhost(_ports[0], listenOptions =>
+                        {
+                            listenOptions.Protocols = HttpProtocols.Http1;
+                        });
+                        options.ListenLocalhost(_ports[1], listenOptions =>
+                        {
+                            listenOptions.Protocols = HttpProtocols.Http2;
+                        });
+                        options.ListenLocalhost(_ports[2], listenOptions =>
+                        {
+                            listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                        });
+                        options.ListenLocalhost(_ports[3], listenOptions =>
+                        {
+                            listenOptions.Protocols = HttpProtocols.Http1;
+                            listenOptions.UseHttps("certificate.pfx");
+                        });
+                        options.ListenLocalhost(_ports[4], listenOptions =>
+                        {
+                            listenOptions.Protocols = HttpProtocols.Http2;
+                            listenOptions.UseHttps("certificate.pfx");
+                        });
+                        options.ListenLocalhost(_ports[5], listenOptions =>
+                        {
+                            listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                            listenOptions.UseHttps("certificate.pfx");
+                        });
+                    })
+                    .Configure(app => {
+                        app.Run(context => context.Response.WriteAsync(context.Request.Protocol));
+                    })
+                    .Build();
+                _ = _host.RunAsync();
+            }
+            void IDisposable.Dispose()
+                => _ = _host.StopAsync();
         }
     }
 }

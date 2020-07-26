3fd41f1024338964272099b62cf86f21114c4219
Dean Ward <dean.ward@bakedbean.org.uk>
2020-07-26 21:57:21 +0100

    Add StackExchange.Utils.Configuration (#15)
    * Add StackExchange.Utils.Configuration
    
    This commit adds a helper library that performs substitution and prefixing for `IConfiguration`-based configuration sources. It allows a value in the configuration tree to reference other values within the configuration system using a placeholder syntax `${key}` or `${nested:key}`. In addition, prefixing allows a "namespace" prefix to be applied to subset of configuration, making it possible to segment configuration into logical areas.
    
    This is particularly useful for storing secrets in a different, secure location but in a way that it makes it easy to compose configuration like connection strings with dealing with it inside the application. E.g. consider the following JSON:
    
    **appsettings.json**
    ```json
    {
        "ConnectionStrings": {
            "Database": "Server=srv01;Database=db01;User ID=${secrets:Database:UserId};Password=${secrets:Database:Password}"
        }
    }
    ```
    
    **secrets.json**
    ```json
    {
        "Database": {
            "UserId": "User123",
            "Password": "Password123!"
        }
    }
    ```
    
    This instructs the substitution provider to lookup the keys `secrets:Database:UserId` and `secrets:Database:Password` from configuration again and replaces them in the value returned for `ConnectionStrings:Database`.
    
    To support this a `ConfigurationBuilder` is configured as follows:
    
    ```c#
    var configuration = new ConfigurationBuilder()
        .WithPrefix(
            "secrets",
            // everything in this configuration builder will be prefixed with "secrets:"
            c => c.AddJsonFile("secrets.json")
        )
        .WithSubstitution(
            // values in this configuration builder will have substitutions
            // replaced prior to being returned to the caller
            c => c.AddJsonFile("appsettings.json")
        )
        .Build();
    ```
    
    Here we're loading a JSON file called `secrets.json` (it could equally be any source supported by the `IConfiguration` system) and prefixing it with `secrets:`. Then, we load `appsettings.json` with support for substitutions. If a caller asks the `IConfiguration` that is produced for a connection string:
    
    ```c#
    var connectionString = configuration.GetConnectionString("Database");
    ```
    
    That value will be returned as `Server=srv01;Database=db01;User ID=User123;Password=Password123!`.
    
    - Build scripts updated to build all projects
    - Builds scripts updated to support Linux/MacOS
    - `.gitignore` tweaks for Rider
    - Move to `LangVersion = latest`
    - `version.json` in each packages directories
    - Use `Microsoft.NETFramework.ReferenceAssemblies` for net462 builds on Linux
    
    * Typos in doc
    
    * Re-enable Kestrel tests that were conditionally compiled out and fix the build breaks
    
    MacOS doesn't support a PKCS12 certificate without a password so `certificate.pfx` has been updated to add a password (`password`). Also MacOS does not support ALPN so HTTP/2 over TLS is not currently available - this commit skips those tests on MacOS.
    
    * Clarifying comment
    
    * Fix up build scripts to use `Build.csproj` instead of individual project files
    
    * Argument validation + tests
    
    * Support `Set` in the prefix and substitution providers, add tests

--- build.ps1
+++ build.ps1
@@ -10,10 +10,7 @@ Write-Host "  CreatePackages: $CreatePackages"
 Write-Host "  RunTests: $RunTests"
 Write-Host "  dotnet --version:" (dotnet --version)
 
-$packageOutputFolder = "$PSScriptRoot\.nupkgs"
-$projectsToBuild =
-    'StackExchange.Utils.Http'
-
+$packageOutputFolder = Join-Path $PSScriptRoot ".nupkgs"
 $testsToRun =
     'StackExchange.Utils.Tests'
 
@@ -23,40 +20,30 @@ if ($PullRequestNumber) {
 }
 
 Write-Host "Building solution..." -ForegroundColor "Magenta"
-dotnet restore ".\StackExchange.Utils.sln" /p:CI=true
-dotnet build ".\StackExchange.Utils.sln" -c Release /p:CI=true
+dotnet build ./Build.csproj -c Release /p:CI=true
 Write-Host "Done building." -ForegroundColor "Green"
 
 if ($RunTests) {
-    foreach ($project in $testsToRun) {
-        Write-Host "Running tests: $project (all frameworks)" -ForegroundColor "Magenta"
-        Push-Location ".\tests\$project"
-
-        dotnet test -c Release
-        if ($LastExitCode -ne 0) {
-            Write-Host "Error with tests, aborting build." -Foreground "Red"
-            Pop-Location
-            Exit 1
-        }
-
-        Write-Host "Tests passed!" -ForegroundColor "Green"
-	    Pop-Location
+    Write-Host "Running tests (all frameworks)" -ForegroundColor "Magenta"
+    dotnet test ./Build.csproj -c Release
+    if ($LastExitCode -ne 0) {
+        Write-Host "Error with tests, aborting build." -Foreground "Red"
+        Exit 1
     }
+    Write-Host "Tests passed!" -ForegroundColor "Green"
 }
 
 if ($CreatePackages) {
-    mkdir -Force $packageOutputFolder | Out-Null
+    if (!(Test-Path $packageOutputFolder)) {
+        New-Item -ItemType Directory $packageOutputFolder | Out-Null
+    }
     Write-Host "Clearing existing $packageOutputFolder..." -NoNewline
-    Get-ChildItem $packageOutputFolder | Remove-Item
+    Get-ChildItem $packageOutputFolder -ErrorAction Ignore | Remove-Item
     Write-Host "done." -ForegroundColor "Green"
 
     Write-Host "Building all packages" -ForegroundColor "Green"
 
-    foreach ($project in $projectsToBuild) {
-        Write-Host "Packing $project (dotnet pack)..." -ForegroundColor "Magenta"
-        dotnet pack ".\src\$project\$project.csproj" --no-build -c Release /p:PackageOutputPath=$packageOutputFolder /p:NoPackageAnalysis=true /p:CI=true
-        Write-Host ""
-    }
+    dotnet pack ./Build.csproj --no-build -c Release -o $packageOutputFolder /p:NoPackageAnalysis=true /p:CI=true
 }
 
 Write-Host "Done."
---
+++ src/StackExchange.Utils.Configuration/SubstitutionHelper.cs
@@ -0,0 +1,169 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+
+namespace StackExchange.Utils
+{
+    /// <summary>
+    /// Helper class that replaces substitution values in a string from <see cref="IConfiguration"/>.
+    /// </summary>
+    public static class SubstitutionHelper
+    {
+        // matches a key wrapped in braces and prefixed with a '$' 
+        // e.g. ${Key} or ${Section:Key} or ${Section:NestedSection:Key}
+        private static readonly Regex _substitutionPattern = new Regex(
+            @"\$\{(?<key>[^\s]+?)\}", RegexOptions.Compiled
+        );
+
+        private static readonly AsyncLocal<ICycleDetector> _cycleDetector = new AsyncLocal<ICycleDetector>();
+
+        /// <summary>
+        /// Replaces substitution placeholders in a value from configuration
+        /// from the provided <see cref="IConfiguration"/> containing values.
+        /// </summary>
+        /// <param name="configuration">
+        /// An <see cref="IConfiguration"/> used to locate the value of <paramref name="key"/>. 
+        /// </param>
+        /// <param name="key">The configuration key.</param>
+        /// <param name="value">
+        /// Value of the key, if found. If it has substitution placeholders it will be returned with them replaced.
+        /// </param>
+        /// <param name="substitutions">
+        /// An <see cref="IConfiguration"/> used as the source of substitution values.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the key was found, <c>false</c> otherwise.
+        /// </returns>
+        internal static bool TryGetWithSubstitution(this IConfiguration configuration, string key, out string value, IConfiguration substitutions)
+        {
+            value = configuration[key];
+            if (value == null)
+            {
+                return false;
+            }
+
+            value = ReplaceSubstitutionPlaceholders(key, value, substitutions, () => new CycleDetector());
+            return true;
+        }
+        
+        /// <summary>
+        /// Replaces substitution placeholders in the specified string with
+        /// values in the provided <see cref="IConfiguration"/>.
+        /// </summary>
+        /// <param name="value">
+        /// A string containing 0 or more substitution placeholders.
+        /// </param>
+        /// <param name="substitutions">
+        /// An <see cref="IConfiguration"/> used as the source of substitution values.
+        /// </param>
+        /// <returns>
+        /// A string with substitutions replaced or unmodified if none were found.
+        /// </returns>
+        public static string ReplaceSubstitutionPlaceholders(string value, IConfiguration substitutions)
+        {
+            return ReplaceSubstitutionPlaceholders(null, value, substitutions, () => NoOpCycleDetector.Instance);
+        }
+        
+        private static string ReplaceSubstitutionPlaceholders(string key, string value, IConfiguration substitutions, Func<ICycleDetector> cycleDetectorFactory)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            
+            // does the value have substitution placeholders in it?
+            var matches = _substitutionPattern.Matches(value);
+            if (matches.Count > 0)
+            {
+                // detect cycles - this can happen when there are multiple
+                // substitutions that reference values in other child providers.
+                // This will end up overflowing the stack so we detect it here
+                // and throw an exception
+                var cycleDetector = _cycleDetector.Value ??= cycleDetectorFactory();
+                
+                cycleDetector.Add(key);
+                
+                // we have some things to substitute, so manipulate the value as a span
+                // so we can slice and dice the substitution values
+                var valueBuilder = new StringBuilder(value.Length);
+                var valueSpan = value.AsSpan();
+                var index = 0;
+
+                try
+                {
+                    foreach (Match match in matches)
+                    {
+                        var substitutionKey = match.Groups["key"].Value;
+                        if (cycleDetector.Contains(substitutionKey))
+                        {
+                            ThrowCycleDetectedError(key, substitutionKey);
+                        }
+
+                        var substitutionValue = substitutions[substitutionKey];
+                        if (substitutionValue != null)
+                        {
+                            // rewrite the value with the substitution
+                            valueBuilder.Append(
+                                valueSpan.Slice(index, match.Index - index)
+                            );
+                            
+                            valueBuilder.Append(substitutionValue);
+                            index = match.Index + match.Length;
+                        }
+                    }
+                }
+                finally
+                {
+                    cycleDetector.Remove(key);
+                }
+                
+                // copy across the remainder of the string
+                valueBuilder.Append(
+                    valueSpan.Slice(index, value.Length - index)
+                );
+
+                value = valueBuilder.ToString();
+            }
+
+            return value;
+        }
+        
+        private static void ThrowCycleDetectedError(string key, string substitutionKey) => throw new InvalidOperationException(
+            $"Cycle detected for when trying to substitute [{substitutionKey}] in [{key}]"
+        );
+        
+        private class CycleDetector : ICycleDetector
+        {
+            private readonly HashSet<string> _values = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+            public void Add(string key) => _values.Add(key);
+
+            public void Remove(string key) => _values.Remove(key);
+
+            public bool Contains(string key) => _values.Contains(key);
+        }
+
+        private class NoOpCycleDetector : ICycleDetector
+        {
+            public static readonly ICycleDetector Instance = new NoOpCycleDetector();
+
+            private NoOpCycleDetector() { }
+
+            public void Add(string key) { }
+
+            public void Remove(string key) { }
+
+            public bool Contains(string key) => false;
+        }
+    
+        private interface ICycleDetector
+        {
+            void Add(string key);
+            void Remove(string key);
+            bool Contains(string key);
+        }
+    }
+}
